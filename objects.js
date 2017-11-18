4e2ee654c64afcaef85b41322fb74eb643240c3d
kent winter <kgw700@gmail.com>
2017-11-18 23:49:26 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  artistname1: "songtitle1",
+  artistname2: "songtitle2",
+  artistname3: "songtitle3"
+};
+ 
+function updatePlaylist(playlist, artistname, songtitle) {
+ return Object.assign({}, playlist, {[artistname]: songtitle})
+}
+
+function removeFromPlaylist(playlist, artistname) {
+ delete playlist.artistname
+ return (playlist)
+}
